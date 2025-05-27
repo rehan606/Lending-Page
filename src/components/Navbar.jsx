381e9f6945cdc2d1985c552e945b3860c0972dfa
unknown <rehanlemu@gmail.com>
2025-05-28 00:09:44 +0600

    Responsive Mobile Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { key } from 'localforage'
 import React, { useState } from 'react'
+import { HiMenu, HiX } from 'react-icons/hi'
 import { href } from 'react-router-dom'
 
 const Navbar = () => {
@@ -22,22 +25,54 @@ const Navbar = () => {
                     <div className='w-4 h-4 bg-red-600 rounded-full opacity-100 hover:opacity-75 transition-opacity -ml-2'></div>
                 </div>
 
+                {/* Mobile menu Button  */}
+                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className='md:hidden p-2'>
+                    {
+                        isMenuOpen ? <HiX className='size-6'/> : <HiMenu className='size-6'/>
+                    }
+                </button>
+
                 {/* Desktop Nav items  */}
                 <div className="hidden md:flex items-center gap-10">
                     {
                     navLinks.map((link, index ) => (
-                        <a key={index} href={link.href} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${activeLink == link.href ? "text-blue-600 after:w-full" : "text-gray-600 hover:text-gray-900"}`}>
+                        <a key={index} href={link.href} onClick={() => setActiveLink(link.href)} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${activeLink == link.href ? "text-blue-600 after:w-full" : "text-gray-600 hover:text-gray-900"}`}>
                             {link.label}
                         </a>
                     ))
                     }
                 </div>
 
-                {/* Cta Button  */}
-
-                {/* Mobile navigation  */}
+                {/* Get in touch Button  */}
+                <button className='hidden md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
+                    <a href="#newsletter">Get in Touch</a>
+                </button>
 
             </div>
+
+            {/* Mobile menu items  */}
+            {
+                isMenuOpen && (
+                    <div className='md:hidden bg-white border-t border-gray-100 py-4 '>
+                        <div className='container mx-auto px-4 space-y-4'>
+                            {
+                                navLinks.map((link, index) => (
+                                    <a key={index} onClick={() => {
+                                        setActiveLink(link.href);
+                                        setIsMenuOpen(false);
+                                        }} 
+                                    className={`block text-sm font-medium py-2 ${activeLink === link.href ? "text-blue-600" 
+                                        : "text-gray-600 hover:text-gray-900"} `} href={link.href}>{link.label}</a>
+                                    ))
+                                }
+
+                                <button className='w-full bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm  font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
+                                    <a href="#newsletter">Get in Touch</a>
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
         </nav>
     )
 }
