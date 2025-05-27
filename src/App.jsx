4555d6982632dfe56b2870e1baf63b13d58b2646
unknown <rehanlemu@gmail.com>
2025-05-28 01:26:29 +0600

    Hero Left section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
       
     </>
   )
