41b915aac494a3d6f4a3069e70a227bbfb32670d
Rehan <rehanlemu@gmail.com>
2025-07-21 16:37:43 +0600

    complete service section

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { BiTime } from 'react-icons/bi'
+import { BsStack } from 'react-icons/bs'
+import { FiSettings } from 'react-icons/fi'
+import { HiLightBulb } from 'react-icons/hi'
+
+const services = [
+    {
+      icon: <BsStack className="w-8 h-8 text-indigo-600" />,
+      title: "Web Design",
+      description: "One for all and all for one, Muskehounds are always ready.",
+      link: "#learn-more"
+    },
+    {
+      icon: <HiLightBulb className="w-8 h-8 text-amber-400" />,
+      title: "Ad-Creatives", 
+      description: "Alphabet Village and the subline of her own road.",
+      link: "#learn-more"
+    },
+    {
+      icon: <FiSettings className="w-8 h-8 text-red-400" />,
+      title: "Automation",
+      description: "Little Blind Text should turn around and return.",
+      link: "#learn-more"
+    },
+    {
+      icon: <BiTime className="w-8 h-8 text-cyan-400" />,
+      title: "Infographics",
+      description: "Nothing the copy said could convince her.",
+      link: "#learn-more"
+    }
+  ]
 
 const ServiceSection = () => {
     return (
         <section className='py-20 container mx-auto px-4 sm:px-6 lg:px-8'>
-            <div className='flex flex-col md:flex-row items-center justify-between gap-10 lg:gap-24'>
+            <div className='flex flex-col md:flex-row items-center justify-between gap-10 '>
                 <div className='md:w-1/3 w-full'>
                     <h2 className='text-3xl md:text-4xl font-bold  mb-6 md:w-4/5  '>Festure of support with new shape</h2> 
                     <p className='text-lg  mb-4 text-gray-600 md:w-4/5'>  Discover your goals, determine success metrics, indentify problems</p> 
@@ -24,6 +55,24 @@ const ServiceSection = () => {
                         </div>
                     </div>
                     <button className='bg-indigo-600 mt-6 hover:bg-indigo-700 transition-colors text-white py-2 px-6 rounded-full'>Get Started</button>
+                </div>
+
+                {/* Service Card  */}
+                <div className=' md:w-2/3 grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    { 
+                        services.map((service, index) => (
+                            <div key={index} className='bg-white mx:w-72 shadow-lg rounded-2xl cursor-poiter hover:scale-105 transition-all  p-6 flex items-start flex-col gap-4 hover:shadow-xl duration-300'>
+                                <div className='text-2xl mb-1'>
+                                    {service.icon}
+                                </div>
+                                <div>
+                                    <h3 className='text-xl font-semibold mb-2'>{service.title}</h3>
+                                    <p className='text-gray-600 mb-4'>{service.description}</p>
+                                    <a href={service.link} className='text-indigo-600 uppercase hover:underline'>Learn More</a>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section> 
